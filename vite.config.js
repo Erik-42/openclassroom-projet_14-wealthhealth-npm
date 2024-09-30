fce320229ce81993bd229f21cba09279262f3ad0
Erik-42 <mesen.erik@gmail.com>
2024-09-30 02:19:14 +0200

    Modif config vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	build: {
+		target: "esnext",
 		lib: {
 			// eslint-disable-next-line no-undef
 			entry: path.resolve(__dirname, "src/lib/index.jsx"),
